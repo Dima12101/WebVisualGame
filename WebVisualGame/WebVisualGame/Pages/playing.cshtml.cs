3685da79a59e35c4de758d7fb70e37aec87ec018
Unknown <oleshkevich19990802@mail.ru>
2019-02-23 00:44:18 +0300

    игровые страницы

---
+++ WebVisualGame/WebVisualGame/Pages/playing.cshtml.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebVisualGame.Data;
+
+namespace WebVisualGame.Pages
+{
+    public class PlayingModel : PageModel
+    {
+		[BindProperty]
+		public string Login { get; set; }
+
+		[BindProperty]
+		public string Password { get; set; }
+
+		private readonly Repository db;
+
+		public PlayingModel(Repository db)
+		{
+			this.db = db;
+		}
+
+		public IActionResult OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+			//db.Users.Add(user);
+			//await db.SaveChangesAsync();
+
+			var user = db.Users.FirstOrDefault(i => i.Login == Login && i.Password == Password);
+
+			if (user != null)
+			{
+				user.ActiveKey = "#keyTest";
+			}
+
+			db.SaveChanges();
+			return RedirectToPage("Index");
+		}
+
+		public void OnGet()
+        {
+        }
+    }
+}
