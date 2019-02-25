9c60f0ca1f214cca45df941c5f1c6747f7d721f6
Unknown <oleshkevich19990802@mail.ru>
2019-02-25 17:23:39 +0300

    begining of playing game

--- WebVisualGame/WebVisualGame/Pages/PlayingIFrame.cshtml.cs
+++ WebVisualGame/WebVisualGame/Pages/PlayingIFrame.cshtml.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.EntityFrameworkCore;
+using System.Web;
+using WebVisualGame.Data;
 namespace WebVisualGame.Pages
 {
     public class PlayingIFrameModel : PageModel
     {
-        public void OnGet()
+
+		private readonly Repository db;
+		private int gameID;
+		private int pointID;
+		private int[] matchingTransition;
+		public String Text { get; private set; }
+
+        public void OnGet(int id_transition)
         {
-        }
+			id_transition = matchingTransition[id_transition];
+			pointID = db.Transitions.FirstOrDefault(i => i.StartPoint == pointID &&
+			i.GameId == gameID &&
+			i.Id == id_transition).NextPoint;
+
+			var matchingTransition = db.Transitions.Select(i => i.StartPoint == pointID &&
+			i.GameId == gameID).ToArray();
+			var l = db.Ñonditions.Select(i => i.TransitionId )
+		}
     }
 }
