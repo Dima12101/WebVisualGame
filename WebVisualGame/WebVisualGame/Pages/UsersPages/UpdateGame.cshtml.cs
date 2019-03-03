4a53f5fb35646da0f7ce4604bcb7214f018b7fb7
Unknown <oleshkevich19990802@mail.ru>
2019-03-03 12:43:33 +0300

    изменил обновление и создание игры.

---
+++ WebVisualGame/WebVisualGame/Pages/UsersPages/UpdateGame.cshtml.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebVisualGame.Data;
+using WebVisualGame.Utilities;
+
+namespace WebVisualGame.Pages
+{
+	public class UpdateGmave : PageModel
+	{
+		[BindProperty]
+		public string ActionForGame { get; set; }
+
+		[BindProperty]
+		public Game Game { get; set; }
+
+		private readonly Repository db;
+
+		[BindProperty]
+		public string Title { get; set; }
+
+		[BindProperty]
+		public string Description { get; set; }
+
+
+		[BindProperty]
+		public IFormFile SourceCode { get; set; }
+
+		[BindProperty]
+		public string SourceCodeContent { get; set; }
+
+		[BindProperty]
+		public string UrlIcon { get; set; }
+
+		[BindProperty]
+		public string Error { get; set; }
+
+		public UpdateGmave(Repository db)
+		{
+			this.db = db;
+		}
+
+		public void OnGet()
+		{
+			SourceCodeContent = Request.Cookies["Content"];
+			Error = Request.Cookies["Error"];
+			Response.Cookies.Delete("Content");
+			Response.Cookies.Delete("Error");
+
+			ActionForGame = "Создать";
+			if (Request.Cookies.ContainsKey("GameId"))
+			{
+				int gameId = Int32.Parse(Request.Cookies["GameId"]);
+				Game = db.Games.FindAsync(gameId);
+				Title = Game.Title;
+				Description = Game.Description;
+				UrlIcon = Game.UrlIcon;
+				ActionForGame = "Обновить";
+			}
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> OnPostSubmit()
+		{
+			// Perform an initial check to catch FileUpload class
+			// attribute violations.
+			if (!ModelState.IsValid)
+			{
+				Response.Cookies.Append("Error", "Ошибка на клиенте");
+				return Page();
+			}
+
+			SourceCodeContent =
+				await FileHelpers.ProcessFormFile(SourceCode, ModelState);
+
+			// Perform a second check to catch ProcessFormFile method
+			// violations.
+			if (!ModelState.IsValid)
+			{
+				Response.Cookies.Append("Error", "Ошибка чтения файла");
+				return Page();
+			}
+
+			var file = new TestFile();
+			file.FileContent = SourceCodeContent;
+			InputGame();
+			Response.Cookies.Delete("GameId");
+			return RedirectToPage("/UserProfil");
+		}
+
+		private void InputGame()
+		{
+			var gameDbWriter = new GameDbWriter(db);
+
+			if (Request.Cookies.ContainsKey("GameId"))
+			{
+				int gameId = Int32.Parse(Request.Cookies["GameId"]);
+				gameDbWriter.UpdateGame(gameId, Title, SourceCodeContent, Description, UrlIcon);
+			}
+			else
+			{
+				gameDbWriter.SaveNewGame(Title, SourceCodeContent, Description, UrlIcon,
+					Int32.Parse(Request.Cookies["UserId"]));
+			}
+		}
+
+		public IActionResult OnPostCancel()
+		{
+			Response.Cookies.Delete("GameId");
+			return RedirectToPage("/UserProfil");
+		}
+	}
+}
--- WebVisualGame/WebVisualGame/Pages/UsersPages/UserProfil.cshtml.cs
+++ WebVisualGame/WebVisualGame/Pages/UsersPages/UserProfil.cshtml.cs
@@ -29,9 +29,6 @@ namespace WebVisualGame.Pages.UsersPages
 		[BindProperty]
 		public Game _Game { get; set; }
 
-		[BindProperty]
-		public string ActionForGame { get; set; }
-
 		private readonly Repository db;
 
 		public UserProfilModel(Repository db)
@@ -45,19 +42,7 @@ namespace WebVisualGame.Pages.UsersPages
 			user = db.Users.FirstOrDefault(i => i.Id == userId);
 			UserName = user.FirstName + " " + user.LastName;
 
-			ActionForGame = "Создать";
-			if (Request.Cookies.ContainsKey("GameId"))
-			{
-				var gameId = int.Parse(Request.Cookies["GameId"]);
-				_Game = db.Games.Find(gameId);
-				ActionForGame = "Обновить";
-			}
-
-			InputField = Request.Cookies.ContainsKey("InputField");
-			if (!InputField)
-			{
-				games = db.Games.Where(i => i.UserId == user.Id).ToList();
-			}
+			games = db.Games.Where(i => i.UserId == user.Id).ToList();
 		}
 
 		public IActionResult OnPostDeleteGame(int gameId)
@@ -70,42 +55,13 @@ namespace WebVisualGame.Pages.UsersPages
 		public IActionResult OnPostUpdateGame(int gameId)
 		{
 			Response.Cookies.Append("GameId", gameId.ToString());
-			Response.Cookies.Append("InputField", "");
-
-			return RedirectToPage();
+			return RedirectToPage("/Updategame");
 		}
 
 		public IActionResult OnPostNewGame()
 		{
 			Response.Cookies.Delete("GameId");
-			Response.Cookies.Append("InputField", "");
-			return RedirectToPage();
-		}
-
-		public IActionResult OnPostInputGame()
-		{
-			var gameDbWriter = new GameDbWriter(db);
-			
-			if (Request.Cookies.ContainsKey("GameId"))
-			{
-				Response.Cookies.Delete("InputField");
-				int gameId = Int32.Parse(Request.Cookies["GameId"]);
-				gameDbWriter.UpdateGame(gameId, _Game.Title, _Game.SourceCode, _Game.Description, _Game.UrlIcon);
-			}
-			else
-			{
-				Response.Cookies.Delete("InputField");
-				gameDbWriter.SaveNewGame(_Game.Title, _Game.SourceCode, _Game.Description, _Game.UrlIcon,
-					Int32.Parse(Request.Cookies["UserId"]));
-			}
-			return RedirectToPage();
-		}
-
-		public IActionResult OnPostCancel()
-		{
-			Response.Cookies.Delete("GameId");
-			Response.Cookies.Delete("InputField");
-			return RedirectToPage();
+			return RedirectToPage("/Updategame");
 		}
 
 		public IActionResult OnPostStartGame(int gameId)
