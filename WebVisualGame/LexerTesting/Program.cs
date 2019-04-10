c8dc17876cbdafb05ebe8e8825ed5a4b0db73d2b
Ignatev Denis <godis7.15@gmail.com>
2019-04-10 21:47:06 +0300

    Old changes

--- WebVisualGame/LexerTesting/Program.cs
+++ WebVisualGame/LexerTesting/Program.cs
@@ -9,6 +9,27 @@ namespace LexerTesting
     {
         static void Main(string[] args)
         {
+            RandomEventGenerator rand = new RandomEventGenerator(0.5, 0.5, 0.9);
+            int i = 0;
+            while (true)
+            {
+                var result = rand.GenerateRandom();
+
+                result.TryGetValue("A", out bool A);
+
+                result.TryGetValue("B", out bool B);
+
+                string Ares = (A) ? "A" : "";
+                string Bres = (B) ? "B" : "";
+
+
+                Console.WriteLine($"Experiment {i}: {Ares} {Bres}");
+                ++i;
+                Console.ReadKey();
+            }
+
+            return;
+
             UTF8Encoding encoder = new UTF8Encoding();
 
             Lexer lexer = new Lexer();
